0c930bb2b802bfa0e9f3fe4c3846cd2ee772b63e
Nicholas Glover <jacksaintjack@gmail.com>
2015-10-20 16:19:24 -0500

    Finished the problems

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 ///////////////////// 1. ///////////////////////////////
 var priceList = items.map(function(p){
+  return p.price;
 });
 
 var averagePrice = priceList.reduce(function(count, total){
@@ -7,7 +8,7 @@ var averagePrice = priceList.reduce(function(count, total){
 });
 
 var answerOne = averagePrice / priceList.length;
-Math.floor(console.log(answerOne));
+console.log("The average Price is " + answerOne.toFixed(2));
 
 
 ///////////////////// 2. ///////////////////////////////
@@ -23,7 +24,7 @@ var answerTwoPrice = targetTwo.map(function(ans){
   return ans.price;
 });
 
-console.log("TITLES:    " + answerTwoTitle + "     PRICES: " +answerTwoPrice);
+console.log('"Items that cost between $14.00 USD and $18.00 USD" ' + answerTwoTitle);
 
 ///////////////////// 3. ///////////////////////////////
 
@@ -39,11 +40,37 @@ console.log(answerThree);
 
 ///////////////////// 4. ///////////////////////////////
 var targetData = items.filter(function(mat){
-  return mat.materials === "wood";
+  return mat.materials.indexOf('wood');
 });
 
 var answerFour = targetData.map(function(ans){
-    return ans.title;
+  return ans.title;
 });
 
 console.log(answerFour);
+
+///////////////////// 5. ///////////////////////////////
+
+var targetFiveOne = items.filter(function(greater){
+  return greater.materials.length >= 8;
+});
+
+var answerFive = targetFiveOne.map(function(ans){
+  return ans.title;
+});
+
+console.log(answerFive);
+
+///////////////////// 6. ///////////////////////////////
+
+var targetSixOne = items.filter(function(maker){
+  return maker.who_made === 'i_did';
+});
+  console.log(targetSixOne.length + " were made by their sellers");
+
+
+//////For Funzies
+var answerSix = targetSixOne.forEach(function(ans){
+  console.log("**THESE WHERE DONE TO TEST forEach**      " + ans.title);
+
+});
